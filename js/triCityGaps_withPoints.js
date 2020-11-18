63534096afe4f4a2b276c1472092b31f0556899f
Dragana <dragana1010@yahoo.ca>
2020-11-18 13:53:44 -0800

    data with points

---
+++ js/triCityGaps_withPoints.js
@@ -0,0 +1,427 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "id": "1",
+      "properties": {
+                "description": "Let's extend the Central Valley Greenway all the way to the Port Mann Bridge.",
+                "location": "Braid Station to Port Mann Bridge, Coquitlam."
+            },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.86155581474303,
+            49.230525077750904
+          ],
+          [
+            -122.83420801162718,
+            49.22871046074081
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "1.1",
+      "properties": {
+                "description": "Let's extend the Central Valley Greenway all the way to the Port Mann Bridge.",
+                "location": "Braid Station to Port Mann Bridge, Coquitlam."
+            },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.8484270721674,
+          49.22965412245079
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "2",
+      "properties": {
+        "description": "This stretch is well served for confident grownups, but none of us is going to ride there with our kids anytime soon. It connects two Skytrain stations, many schools, two civic libraries, two city halls, multiple recreation complexes, multiple parks. It needs a separated lane on the north side, where there is a lot of room and very few cross-streets.",
+        "location": "Guildford Way, Coquitlam City Hall to Port Moody City Hall."
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.79242992401122,
+            49.284883609895566
+          ],
+          [
+            -122.79657125473021,
+            49.28519153864841
+          ],
+          [
+            -122.79906034469604,
+            49.28527551888364
+          ],
+          [
+            -122.801034450531,
+            49.285205535364206
+          ],
+          [
+            -122.80208587646484,
+            49.28517754192863
+          ],
+          [
+            -122.80397415161133,
+            49.28509356152657
+          ],
+          [
+            -122.8056263923645,
+            49.28498158743462
+          ],
+          [
+            -122.80723571777344,
+            49.284757638487875
+          ],
+          [
+            -122.80880212783813,
+            49.28456168232514
+          ],
+          [
+            -122.81008958816528,
+            49.28426774662085
+          ],
+          [
+            -122.81137704849243,
+            49.28386183252964
+          ],
+          [
+            -122.81249284744261,
+            49.28352590109962
+          ],
+          [
+            -122.81384468078613,
+            49.28332994004192
+          ],
+          [
+            -122.81551837921144,
+            49.283511903907026
+          ],
+          [
+            -122.8169560432434,
+            49.283959812098956
+          ],
+          [
+            -122.81923055648804,
+            49.28474364164494
+          ],
+          [
+            -122.82032489776611,
+            49.28482762264301
+          ],
+          [
+            -122.82144069671632,
+            49.28457567921971
+          ],
+          [
+            -122.82225608825684,
+            49.28418376466922
+          ],
+          [
+            -122.82300710678099,
+            49.28363787849724
+          ],
+          [
+            -122.82380104064941,
+            49.28311998090162
+          ],
+          [
+            -122.82483100891113,
+            49.28237811811285
+          ],
+          [
+            -122.82742738723755,
+            49.280782375425375
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "2.1",
+      "properties": {
+        "description": "This stretch is well served for confident grownups, but none of us is going to ride there with our kids anytime soon. It connects two Skytrain stations, many schools, two civic libraries, two city halls, multiple recreation complexes, multiple parks. It needs a separated lane on the north side, where there is a lot of room and very few cross-streets.",
+        "location": "Guildford Way, Coquitlam City Hall to Port Moody City Hall."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.80857145786285,
+          49.284596674554166
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "3",
+      "properties": {
+        "location": "Pitt River Bridge to Coquitlam Center/ Lincoln Station, PoCo/ Coquitlam."
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.73123264312743,
+            49.24878664227208
+          ],
+          [
+            -122.79414653778076,
+            49.28046042107946
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "3.1",
+      "properties": {
+        "location": "Pitt River Bridge to Coquitlam Center/ Lincoln Station, PoCo/ Coquitlam."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.76109904050827,
+          49.26382529551108
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "4",
+      "properties": {
+        "description": "Let's connect the Barnet Highway to Rocky Point Park and Moody Central Station!",
+        "location": "Clarke Road, Port Moody."
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.86682367324829,
+            49.277716724857335
+          ],
+          [
+            -122.84590780735016,
+            49.27795470455463
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "4.1",
+      "properties": {
+        "description": "Let's connect the Barnet Highway to Rocky Point Park and Moody Central Station!",
+        "location": "Clarke Road, Port Moody."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.85662055015564,
+          49.27783287134203
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "5",
+      "properties": {
+        "description": "This important transportation route is dead flat. Let's create a great bike connection here!",
+        "location": "Coquitlam Central Station to Port Mann Bridge, Coquitlam."
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.79951095581053,
+            49.273964893181876
+          ],
+          [
+            -122.815260887146,
+            49.22398794036374
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "5.1",
+      "properties": {
+        "description": "This important transportation route is dead flat. Let's create a great bike connection here!",
+        "location": "Coquitlam Central Station to Port Mann Bridge, Coquitlam."
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.80705533921717,
+          49.25003150305601
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "6",
+      "properties": {
+                "description": "Let's create connection to Sasamat Lake. Regional Greenway Plan 2050 has also identified the Sasamat Greenway gap.",
+                "location": "Alderside to Sasamat Lake/Belcarra Park."
+            },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8648281097412,
+            49.299214255388144
+          ],
+          [
+            -122.8691840171814,
+            49.30125715262291
+          ],
+          [
+            -122.86974191665648,
+            49.30173288365767
+          ],
+          [
+            -122.87014961242677,
+            49.30211066679605
+          ],
+          [
+            -122.87049293518066,
+            49.30232054506593
+          ],
+          [
+            -122.87094354629515,
+            49.3024464715988
+          ],
+          [
+            -122.8716731071472,
+            49.30253042244195
+          ],
+          [
+            -122.8734540939331,
+            49.30267034019601
+          ],
+          [
+            -122.87349700927734,
+            49.30654590406828
+          ],
+          [
+            -122.87347555160522,
+            49.307441297858
+          ],
+          [
+            -122.8736686706543,
+            49.30913410727741
+          ],
+          [
+            -122.87388324737547,
+            49.30948385403957
+          ],
+          [
+            -122.87426948547363,
+            49.30984758803837
+          ],
+          [
+            -122.87463426589966,
+            49.31002945403067
+          ],
+          [
+            -122.87510633468628,
+            49.31018334011518
+          ],
+          [
+            -122.87577152252197,
+            49.310295256965624
+          ],
+          [
+            -122.87632942199707,
+            49.31044914221986
+          ],
+          [
+            -122.8766942024231,
+            49.310672974458825
+          ],
+          [
+            -122.87695169448851,
+            49.310938773921706
+          ],
+          [
+            -122.87609338760376,
+            49.31324697211094
+          ],
+          [
+            -122.87615776062012,
+            49.31358270101957
+          ],
+          [
+            -122.87645816802977,
+            49.31393241619951
+          ],
+          [
+            -122.87686586380003,
+            49.31414224411574
+          ],
+          [
+            -122.87746667861937,
+            49.31435207113811
+          ],
+          [
+            -122.87808895111084,
+            49.31442201328028
+          ],
+          [
+            -122.87901163101198,
+            49.314436001696784
+          ],
+          [
+            -122.8891611099243,
+            49.31396039330665
+          ],
+          [
+            -122.88978338241577,
+            49.31414224411574
+          ],
+          [
+            -122.89010524749756,
+            49.31449195532312
+          ],
+          [
+            -122.89012670516966,
+            49.314799699131825
+          ],
+          [
+            -122.89001941680908,
+            49.31519137028941
+          ],
+          [
+            -122.88976192474364,
+            49.316086606954464
+          ],
+          [
+            -122.8895366191864,
+            49.31673704213806
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "6.1",
+      "properties": {
+                "description": "Let's create connection to Sasamat Lake. Regional Greenway Plan 2050 has also identified the Sasamat Greenway gap.",
+                "location": "Alderside to Sasamat Lake/Belcarra Park."
+            },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.8750754892826,
+          49.31017372224899
+        ]
+      }
+    }
+  ]
+}
