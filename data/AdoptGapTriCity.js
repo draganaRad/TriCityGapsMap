2dd87e7985cfc7bdeb6d8bc24f7b4acd41498b34
Dragana Radulovic <draganaradulovic@Draganas-MBP.vc.shawcable.net>
2021-01-16 10:30:09 -0800

    added adopt-a-gap data

---
+++ data/AdoptGapTriCity.js
@@ -0,0 +1,80 @@
+var adoptGaps = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Gap 920 adopted: Burns from Meridian to Dominion",
+                "Description": "Learn why Andrew Browne generously adopted this gap below:<br><br>\"This is a major link between two \"backways\" for bikes, and it desperately needs improvement.\""
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.7281262,
+                    49.2650649,
+                    0
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Gap 933 adopted: Guildford Way from Carlsen to Pinetree Way",
+                "Description": "Learn why Marc Fernandez generously adopted this gap below:<br><br>\"To improve cycling connections and safety in the Tri-Cities and across Canada (as this is part of The Great Trail). For weekend and daily commuter cyclists.\"<br><br>You too can Adopt-a-Gap here: <a href=\"<a href=\"https://bikehub.ca/get-involved/ungapthemap/adopt-gap\" target=\"_blank\">https://bikehub.ca/get-involved/ungapthemap/adopt-gap</a>"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.81039,
+                    49.2841278,
+                    0
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Gap 905 adopted: Kingsway MUP to Mary Hill Bypass",
+                "Description": "This gap has been generously adopted by Uliana. Learn more about her story here: <br> https://bikehub.ca/blog/uliana-is-helping-create-connected-cycling-network-and-you-can-too <br><br>You too can Adopt-a-Gap here: <a href=\"https://bikehub.ca/get-involved/ungapthemap/adopt-gap\" target=\"_blank\">https://bikehub.ca/get-involved/ungapthemap/adopt-gap</a> "
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.7594418,
+                    49.2515471,
+                    0
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Gap 903 Adopted - Major Improvement: Evergreen Line Bikeway",
+                "Description": "Learn why Robert McMurtry adopted this gap:<br><br>\"The current route down what's really an alley between St. John's and Clarke is unsafe and inefficient. Broken pavement, speed bumps, unlit, multiple crossings. Heading west is a ridiculous mess. Let's make bike travel safe and efficient from the overpass to the Barnet.\"<br><br>You too can adopt a gap here: <a href=\"https://bikehub.ca/get-involved/ungapthemap/adopt-gap\" target=\"_blank\">https://bikehub.ca/get-involved/ungapthemap/adopt-gap</a>"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.8584017,
+                    49.2780125,
+                    0
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Name": "Gap 33 adopted: North Rd from Broadway to the CVG",
+                "Description": "Learn why Glenn Eriksen generously adopted this gap: <br><br>\"North/South routes in Burnaby are limited by the Trans-Canada Highway and North Road is an important link which otherwise requires significant detours to avoid. With massive development progressing in the Lougheed Area and Burquitlam, it's more critical than ever.\"<br><br>You too can Adopt-a-Gap here: <a href=\"https://bikehub.ca/get-involved/ungapthemap/adopt-gap\" target=\"_blank\">https://bikehub.ca/get-involved/ungapthemap/adopt-gap</a>"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.8928198,
+                    49.2472056,
+                    0
+                ]
+            }
+        }
+    ]
+}
